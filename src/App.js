4213bde648b851243533e52f6b365e2a7ab445ea
Portgas D Ace <72243953+afnan007a@users.noreply.github.com>
2021-10-29 21:11:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,34 @@
 import './App.css';
 import logo from './logo.png';
-
+import banner from './banner.svg';
 
 function App() {
   return (
-<div className="App">
-  <header className="App-header">
-    <div class="underline">
-      <h1> <code><strong> Welcome to Catto's Portfolio</strong> </code></h1>
-    </div>
-    <h3><code><strong>Meow! I'm catto, want some hugs?</strong></code></h3>
-    <img src={logo} className="App-logo" alt="logo" />
+<div class="App">
+  <div class="banner">
+    <a href="https://www.youtube.com/watch?v=eCOdMdWbP_4">
+      <p align="right"> <img src={banner} height="70" class="banner" filter="drop-shadow(20px 20px 20px)" alt="banner" /></p>
+    </a>
+    <h1 align="center" class="banner"> <code><strong> 🤎 Welcome to Catto's Portfolio 🤎</strong> </code></h1> </div>
+  <header class="App-header">
+    <h3><code><strong>Meow! I'm catto, want some hugs?</strong></code></h3> <img src={logo} className="App-logo" alt="logo" />
     <div class="container">
       <div class="box">
-        <p>
-          <strong>‏‏‎ ‎‏‏‎ ‎‏‏‎ ‏‏‎ ‎‏‏‎ ‎‏‏‎ <code>‎‎‎‎Want to know more about me?</code> ‎‏‏‎ ‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‎</strong>
-        </p>
-        <p>
-          <strong>‏‏‎ ‎‏‏‎ ‎‏‏‎ ‏‏‎ ‎‏‏‎ ‎‏‏‎ <code>Click the button below!</code> ‎‏‏‎ ‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‎</strong>
-        </p>
-        <a
-          className="App-link"
-          href="https://www.youtube.com/watch?v=eCOdMdWbP_4"
-          target="_blank"
-          rel="noopener noreferrer"
-          >
-          <p><button class="btn btn1"><code> <strong>Learn More!</strong></code></button></p>
+        <p> <strong>‏‏‎ ‎‏‏‎ ‎‏‏‎ ‏‏‎ ‎‏‏‎ ‎‏‏‎ <code>‎‎‎‎Want to know more about me?</code> ‎‏‏‎ ‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‎</strong> </p>
+        <p> <strong>‏‏‎ ‎‏‏‎ ‎‏‏‎ ‏‏‎ ‎‏‏‎ ‎‏‏‎ <code>Click the button below!</code> ‎‏‏‎ ‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‎</strong> </p>
+        <a href="https://www.youtube.com/watch?v=eCOdMdWbP_4">
+          <p>
+            <button class="btn btn1"><code> <strong>Learn More!</strong></code></button>
+          </p>
         </a>
-        <p>  ‏‏‎ ‎‏‏‎ ‎‏‏‎ ‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‎</p>
+        <p> ‏‏‎ ‎‏‏‎ ‎‏‏‎ ‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‎</p>
       </div>
     </div>
     <div class="footer">
-      <h4><code>😻 “There are two means of refuge from the miseries of life: music and cats.” – Albert Schweitzer</code></h4>
-    </div>
+      <h4><code>🤎 “There are two means of refuge from the miseries of life: music and cats.” – Albert Schweitzer 🤎</code></h4> </div>
   </header>
 </div>
       );
     }
     
     export default App;
-
